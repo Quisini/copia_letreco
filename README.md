4122f89743239f96e09ec7d6621620698dec1e6a
Gustavo Quisini <163922998+Quisini@users.noreply.github.com>
2025-03-11 00:34:05 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 Cópia do jogo LETRECO/WORDLE
-Contém 6 tentativas para adivinhar uma palavra. A palavra pode ser uma aleatória em uma lista de 20.
+Contém 6 tentativas para adivinhar uma palavra. A palavra pode ser uma aleatória de uma lista de 20. Cada refresh muda a palavra.
+Você pode usar enter para admitir a palavra.
 
 Feito usando HTML, CSS e JavaScript Vanilla
 
--- script.js
+++ script.js
@@ -1,88 +1,117 @@
 const palavras = [
-    "CASAS", "BOLAS", "LIVRO", "FRUTA", "FLORA",
-    "PRAIA", "CARRO", "TORRE", "VIDRO", "PEIXE",
-    "PEDRA", "LUZES", "CORPO", "AMIGO", "SABOR",
-    "FLOCO", "TERRA", "FOGOS", "NUVEM", "LADOS",
-    "VIOLA", "SONHO", "DENTE", "GRAMA", "FESTA"
-  ];
+  "CASAS",
+  "BOLAS",
+  "LIVRO",
+  "FRUTA",
+  "FLORA",
+  "PRAIA",
+  "CARRO",
+  "TORRE",
+  "VIDRO",
+  "PEIXE",
+  "PEDRA",
+  "LUZES",
+  "CORPO",
+  "AMIGO",
+  "SABOR",
+  "FLOCO",
+  "TERRA",
+  "FOGOS",
+  "NUVEM",
+  "LADOS",
+  "VIOLA",
+  "SONHO",
+  "DENTE",
+  "GRAMA",
+  "FESTA",
+];
 
-const input = document.getElementById('entrada');
+const input = document.getElementById("entrada");
 const palavra = document.querySelectorAll(".palavra");
+const textWrongDiv = document.getElementById("textWrongDiv");
+const certo = palavras[Math.floor(Math.random() * palavras.length)];
+const button = document.getElementById("button");
 var cont = 0;
 var ativo = true;
 
-const certo = palavras[(Math.floor(Math.random() * palavras.length))];
+document.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    button.click();
+  }
+});
+
+button.addEventListener("click", testar);
+
 console.log(certo);
 
-function testar()
-{
-    if(cont > 5)
-    {
-        alert("ACABARAM AS CHANCES, A PALAVRA ERA " + certo);
-        ativo = false;
-        input.disabled = true;
-        return 0;
+function testar() {
+  console.log(cont);
+  var tentativa = input.value.toUpperCase();
+
+  if (ativo) {
+    if (tentativa == certo) {
+      alert("PARABÉNS, VOCÊ ACERTOU A PALAVRA!");
+      ativo = false;
+      input.disabled = true;
     }
 
-    var tentativa = input.value.toUpperCase();
+    var arrLetras = palavra[cont].querySelectorAll(".letra");
+
+    if (tentativa.length != 5) {
+      //   alert("PALAVRA INVÁLIDA, ELA DEVE CONTER 5 LETRAS");
+      if (!textWrongDiv.hasChildNodes()) {
+        let wrongTextMessage = document.createElement("p");
+        wrongTextMessage.classList.add("text__wrong");
+        wrongTextMessage.textContent = "A palavra deve conter 5 letras";
+        textWrongDiv.appendChild(wrongTextMessage);
+      }
+      return;
+    } else if (tentativa.length == 5 && textWrongDiv.hasChildNodes()) {
+      textWrongDiv.removeChild(textWrongDiv.children[0]);
+    }
+
+    //Colocando as letras na palavra
+    arrLetras.forEach((elementLetra, index) => {
+      elementLetra.textContent = tentativa[index];
+      let textoLetra = elementLetra.textContent;
+      if (certo.includes(textoLetra)) {
+        index == certo.indexOf(textoLetra, index)
+          ? elementLetra.classList.add("verde")
+          : elementLetra.classList.add("amarelo");
+      } else {
+        elementLetra.classList.add("vermelho");
+      }
+    });
+
+    //Checando cada palavra
+    // letras.forEach((letra) => {
+    //   letra.classList.remove("vermelho");
+    //   letra.classList.remove("verde");
+    //   letra.classList.remove("amarelo");
+    // });
+
+    // for (var i = 0; i < 5; i++) {
+    //   for (var j = 0; j < 5; j++) {
+    //     if (tentativa[j] == certo[i]) {
+    //       if (i == j) {
+    //         letras[j].classList.add("verde");
+    //         break;
+    //       } else {
+    //         letras[j].classList.add("amarelo");
+    //       }
+    //     }
+    //   }
+    // }
+
     input.value = "";
 
-    if (ativo)
-    {
-        if (tentativa == certo)
-        {
-            alert("PARABÉNS, VOCÊ ACERTOU A PALAVRA!");
-            ativo = false;
-            input.disabled = true;
-        }
-
-        var letras = palavra[cont].querySelectorAll(".letra");
-
-        if (tentativa.length != 5)
-        {
-            alert("PALAVRA INVÁLIDA, ELA DEVE CONTER 5 LETRAS");
-            input.value = "";
-            return 1;
-        }
-
-        //Colocando as letras na palavra
-        for(var i = 0; i < 5; i++)
-        {
-            letras[i].textContent = tentativa[i];
-        }
-
-        //Checando cada palavra
-        letras.forEach(letra => {
-            letra.classList.add('vermelho');
-        });
-
-        for (var i = 0; i < 5; i++)
-        {
-            for(var j = 0; j < 5; j++)
-            {
-                if(tentativa[j] == certo[i])
-                {
-                    if (i == j)
-                    {
-                        letras[j].classList.add('verde');
-                        letras[j].classList.remove('vermelho');
-                        break;
-                    }
-                    else 
-                    {
-                        letras[j].classList.add('amarelo');
-                        letras[j].classList.remove('vermelho');
-                    }
-                }
-            }
-        }
-
-        if(cont >= 5 && ativo == true)
-        {
-            alert("ACABARAM AS CHANCES, A PALAVRA ERA " + certo);
-            ativo = false;
-            input.disabled = true;
-        }
-        cont++;
+    if (cont <= 4 && ativo) {
+      cont++;
+    } else {
+      alert("ACABARAM AS CHANCES, A PALAVRA ERA " + certo);
+      ativo = false;
+      input.disabled = true;
     }
+  }
 }
